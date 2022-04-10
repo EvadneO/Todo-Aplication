3e8094fc655f0a97809da3f4fccf9d147d785fe0
EvadneO <evadne28@gmail.com>
2022-04-10 22:49:58 +0000

    Cambios de todo list

--- src/js/component/controlador.jsx
+++ src/js/component/controlador.jsx
@@ -1,51 +1,17 @@
 import React, { useState } from "react";
 
 function Control() {
-	const [tarea, setTarea] = React.useState("");
-	const [tareas, setTareas] = React.useState([]);
-	const [modoEdicion, setModoEdicion] = React.useState(false);
-	const [id, setId] = React.useState("");
-	const [error, setError] = React.useState(null);
+	const [tarea, setTarea] = useState("");
+	const [tareas, setTareas] = useState([]);
 
-	const agregarTarea = (e) => {
-		e.preventDefault();
-		if (!tarea.trim()) {
-			console.log("Campo vacio");
-			setError("El campo no puede estar Vacío");
-			return;
-		}
-		setTareas([...tareas, { tarea, id: shortid.generate() }]);
-		setTarea("");
-		setError(null);
+	const lista = (hola) => {
+		hola.preventDefault();
+		setTareas([...tareas, tarea]);
 	};
 
-	const eliminarTarea = (id) => {
-		const arrayFiltrado = tareas.filter((item) => item.id !== id);
-		setTareas(arrayFiltrado);
-	};
-
-	const editar = (item) => {
-		setModoEdicion(true);
-		setTarea(item.tarea);
-		setId(item.id);
-	};
-
-	const editarTarea = (e) => {
-		e.preventDefault();
-		if (!tarea.trim()) {
-			console.log("Campo vacio");
-			setError("El campo no puede estar vacío");
-			return;
-		}
-
-		const arrayEditado = tareas.map((item) =>
-			item.id === id ? { id, tarea } : item
-		);
-		setTareas(arrayEditado);
-		setModoEdicion(false);
-		setTarea("");
-		setId("");
-		setError(null);
+	const eliminar = (key) => {
+		setTareas(tareas.filter((item, index) => index !== key));
+		console.log(key);
 	};
 
 	return (
@@ -53,37 +19,7 @@ function Control() {
 			<h1 className="text-center">TAREAS</h1>
 			<div className="row">
 				<div className="col-10">
-					<h4 className="text-center">Lista de Tareas</h4>
-					<ul className="list-group">
-						{tareas.length === 0 ? (
-							<li className="list-group-item">Sin Tareas</li>
-						) : (
-							tareas.map((item) => (
-								<li className="list-group-item" key={item.id}>
-									<span className="lead">{item.tarea}</span>
-									<button
-										className="btn btn-sm btn-danger float-right mx-2"
-										onClick={() => eliminarTarea(item.id)}>
-										Eliminar
-									</button>
-									<button
-										className="btn btn-sm btn-warning float-right"
-										onClick={() => editar(item)}>
-										Editar
-									</button>
-								</li>
-							))
-						)}
-					</ul>
-				</div>
-				<div className="col-2">
-					<h4 className="text-center">
-						{modoEdicion ? "Editar Tarea" : "Agregar"}
-					</h4>
-					<form onSubmit={modoEdicion ? editarTarea : agregarTarea}>
-						{error ? (
-							<span className="text-danger">{error}</span>
-						) : null}
+					<form onSubmit={lista}>
 						<input
 							type="text"
 							className="form-control mb-2"
@@ -91,19 +27,22 @@ function Control() {
 							onChange={(e) => setTarea(e.target.value)}
 							value={tarea}
 						/>
-						{modoEdicion ? (
-							<button
-								className="btn btn-warning btn-block"
-								type="submit">
-								Editar
-							</button>
-						) : (
-							<button
-								className="btn btn-dark btn-block"
-								type="submit">
-								Agregar
-							</button>
-						)}
+						<ul className="list-group">
+							{tareas.map((li, key) => (
+								<li className="list-group-item" key={key}>
+									{li}s
+								</li>
+							))}
+							{tareas.map((li, key) => (
+								<span
+									className="close"
+									onClick={() => eliminar(key)}>
+									<button type="button" class="btn btn-dark">
+										Eliminar
+									</button>
+								</span>
+							))}
+						</ul>
 					</form>
 				</div>
 			</div>
